3f1a194687c665c40faad6b15afeb22cc7d1fdfb
mojtaba013 <83921100+mojtaba013@users.noreply.github.com>
2023-03-18 22:20:39 +0330

    1401/12/27 work on login modal

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -484,10 +484,10 @@ const Navigation = () => {
                 <div
                   className={`absolute w-72 h-48   z-20 top-10 left-5 rounded-md transform ${
                     closeModal ? "scale-100" : "scale-0"
-                  } transition duration-500 bg-white`}
+                  } transition duration-[400ms] bg-white`}
                 >
-                  <div className="flex flex-col p-6 text-slate-500">
-                    <div className="flex  items-center gap-x-2 border-b pb-2 mb-2">
+                  <div className="flex flex-col  p-6 text-slate-500">
+                    <div className="flex  items-center gap-x-2 border-b pb-2 mb-3">
                       <img
                         alt="account"
                         className="w-8 h-8 rounded-full"
@@ -499,10 +499,26 @@ const Navigation = () => {
                         <div>{auth.email}</div>
                       </div>
                     </div>
-
+                    <div className="flex cursor-pointer gap-x-1 mb-4 ">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                        />
+                      </svg>
+                      <span>لیست علاقه مندیها</span>
+                    </div>
                     <div
                       onClick={exitAccountHandler}
-                      className="flex cursor-pointer"
+                      className="flex cursor-pointer  gap-x-1"
                     >
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -518,9 +534,9 @@ const Navigation = () => {
                           d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
                         />
                       </svg>
-
                       <p>خروج</p>
                     </div>
+                  
                   </div>
                 </div>
               </>
@@ -682,10 +698,10 @@ const Navigation = () => {
                 <div
                   className={`absolute w-72 h-48   z-20 top-10 left-5 rounded-md transform ${
                     closeModal ? "scale-100" : "scale-0"
-                  } transition duration-500 bg-white`}
+                  } transition duration-[400ms] bg-white`}
                 >
                   <div className="flex flex-col p-6 text-slate-500">
-                    <div className="flex  items-center gap-x-2 border-b pb-2 mb-2">
+                    <div className="flex  items-center gap-x-2 border-b pb-2 mb-3">
                       <img
                         alt="account"
                         className="w-8 h-8 rounded-full"
@@ -698,9 +714,26 @@ const Navigation = () => {
                       </div>
                     </div>
 
+                    <div className="flex cursor-pointer gap-x-1 mb-4 ">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                        />
+                      </svg>
+                      <span>لیست علاقه مندیها</span>
+                    </div>
                     <div
                       onClick={exitAccountHandler}
-                      className="flex cursor-pointer"
+                      className="flex cursor-pointer  gap-x-1"
                     >
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -716,7 +749,6 @@ const Navigation = () => {
                           d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
                         />
                       </svg>
-
                       <p>خروج</p>
                     </div>
                   </div>
